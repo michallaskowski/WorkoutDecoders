a0a9101ca1d618ea608240998273d04748a37fbf
Michal Laskowski <michal@michallaskowski.dev>
2021-02-24 19:08:40 +0100

    Add FreeRide to tests

--- Tests/WorkoutDecodersTests/WorkoutDecodersTests.swift
+++ Tests/WorkoutDecodersTests/WorkoutDecodersTests.swift
@@ -15,16 +15,17 @@ final class WorkoutDecodersTests: XCTestCase {
         XCTAssertEqual(workout.segments, [
             WorkoutSegment(duration: 10, index: 0, intervalIndex: nil, powerStart: 0.5, powerEnd: 0.55),
             WorkoutSegment(duration: 5, index: 1, intervalIndex: nil, powerStart: 0.4, powerEnd: nil),
-            WorkoutSegment(duration: 10, index: 2, intervalIndex: nil, powerStart: 0.5, powerEnd: nil),
+            WorkoutSegment(duration: 100, index: 2, intervalIndex: nil, powerStart: -1, powerEnd: nil), // free ride
+            WorkoutSegment(duration: 10, index: 3, intervalIndex: nil, powerStart: 0.5, powerEnd: nil),
             // interval start, repeat 3
-            WorkoutSegment(duration: 15, index: 3, intervalIndex: 0, powerStart: 1.2, powerEnd: nil),
-            WorkoutSegment(duration: 5, index: 4, intervalIndex: 0, powerStart: 0.4, powerEnd: nil),
-            WorkoutSegment(duration: 15, index: 5, intervalIndex: 1, powerStart: 1.2, powerEnd: nil),
-            WorkoutSegment(duration: 5, index: 6, intervalIndex: 1, powerStart: 0.4, powerEnd: nil),
-            WorkoutSegment(duration: 15, index: 7, intervalIndex: 2, powerStart: 1.2, powerEnd: nil),
-            WorkoutSegment(duration: 5, index: 8, intervalIndex: 2, powerStart: 0.4, powerEnd: nil),
+            WorkoutSegment(duration: 15, index: 4, intervalIndex: 0, powerStart: 1.2, powerEnd: nil),
+            WorkoutSegment(duration: 5, index: 5, intervalIndex: 0, powerStart: 0.4, powerEnd: nil),
+            WorkoutSegment(duration: 15, index: 6, intervalIndex: 1, powerStart: 1.2, powerEnd: nil),
+            WorkoutSegment(duration: 5, index: 7, intervalIndex: 1, powerStart: 0.4, powerEnd: nil),
+            WorkoutSegment(duration: 15, index: 8, intervalIndex: 2, powerStart: 1.2, powerEnd: nil),
+            WorkoutSegment(duration: 5, index: 9, intervalIndex: 2, powerStart: 0.4, powerEnd: nil),
             // interval end
-            WorkoutSegment(duration: 5, index: 9, intervalIndex: nil, powerStart: 0.6, powerEnd: nil)
+            WorkoutSegment(duration: 5, index: 10, intervalIndex: nil, powerStart: 0.6, powerEnd: nil)
         ])
     }
 
